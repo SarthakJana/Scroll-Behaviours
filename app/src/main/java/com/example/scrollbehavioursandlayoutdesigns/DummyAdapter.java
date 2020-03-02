9746dc083845467dcd0bd222ed00706b61732d17
Sarthak Jana <sarthakjana09@gmail.com>
2020-03-02 13:43:18 +0530

    Palette API, Ripple Animation

--- app/src/main/java/com/example/scrollbehavioursandlayoutdesigns/DummyAdapter.java
+++ app/src/main/java/com/example/scrollbehavioursandlayoutdesigns/DummyAdapter.java
@@ -1,17 +1,24 @@
 package com.example.scrollbehavioursandlayoutdesigns;
 
+import android.animation.Animator;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class DummyAdapter extends RecyclerView.Adapter {
 
     private Context mContext;
+
 
     public DummyAdapter(Context mContext) {
         this.mContext = mContext;
@@ -34,13 +41,49 @@ public class DummyAdapter extends RecyclerView.Adapter {
         return 20;
     }
 
-    public static class DummyCell extends RecyclerView.ViewHolder {
+
+    public static class DummyCell extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private static int green;
+        private static int white;
+        private static int black;
 
         TextView textView;
 
         public DummyCell(@NonNull View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.tv_stanberg);
+            itemView.setOnClickListener(this);
+            black = itemView.getContext().getResources().getColor(R.color.black);
+
+            if (green == 0)
+                green = itemView.getContext().getResources().getColor(R.color.colorPrimary);
+            if (white == 0)
+                white = itemView.getContext().getResources().getColor(R.color.background_material_light);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onClick(View view) {
+            boolean isSelected = view.getBackground() != null && ((ColorDrawable) view.getBackground()).getColor() == green;
+
+            int finalRadius = (int) Math.hypot(view.getWidth() / 2, view.getHeight() / 2);
+
+            if (isSelected) {
+                view.setBackgroundColor(white);
+                textView.setTextColor(black);
+            } else {
+                Animator animation = ViewAnimationUtils.createCircularReveal(
+                        view,
+                        view.getWidth() / 2,
+                        view.getHeight() / 2,
+                        0,
+                        finalRadius);
+
+                view.setBackgroundColor(green);
+                textView.setTextColor(white);
+                animation.start();
+            }
         }
     }
 }
--- app/src/main/java/com/example/scrollbehavioursandlayoutdesigns/ParallaxWithTabActivity.java
+++ app/src/main/java/com/example/scrollbehavioursandlayoutdesigns/ParallaxWithTabActivity.java
@@ -1,12 +1,18 @@
 package com.example.scrollbehavioursandlayoutdesigns;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.palette.graphics.Palette;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 public class ParallaxWithTabActivity extends AppCompatActivity {
@@ -14,6 +20,8 @@ public class ParallaxWithTabActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private FloatingActionButton mFabButton;
+    private int fabColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,7 @@ public class ParallaxWithTabActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tab_with_parallax);
         mViewPager = findViewById(R.id.viewPager);
         mTabLayout = findViewById(R.id.tabLayout);
+        mFabButton = findViewById(R.id.fab);
         mCollapsingToolbarLayout = findViewById(R.id.collapsibleToolbarLayout);
         mCollapsingToolbarLayout.setTitle("Walchensee");
         mCollapsingToolbarLayout.setExpandedTitleTextColor(ColorStateList.valueOf(getResources().getColor(R.color.white)));
@@ -32,5 +41,30 @@ public class ParallaxWithTabActivity extends AppCompatActivity {
         viewPagerAdapter.addFragment(new DummyFragment(), "Tab3");
         mViewPager.setAdapter(viewPagerAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
+
+        final int defaultColor = getResources().getColor(R.color.colorPrimaryDark);
+
+        try {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.stanberg_see);
+            Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                @Override
+                public void onGenerated(@Nullable Palette palette) {
+                    int mutedColor = palette.getMutedColor(defaultColor);
+                    int fabColor = palette.getDarkVibrantColor(defaultColor);
+                    int vibrantDarkColor = palette.getDarkMutedColor(defaultColor);
+                    mCollapsingToolbarLayout.setContentScrimColor(mutedColor);
+                    mCollapsingToolbarLayout.setStatusBarScrimColor(vibrantDarkColor);
+                    mFabButton.setBackgroundTintList(ColorStateList.valueOf(fabColor));
+                }
+            });
+        } catch (Exception e) {
+            mCollapsingToolbarLayout.setContentScrimColor(
+                    ContextCompat.getColor(this, R.color.colorPrimary)
+            );
+            mCollapsingToolbarLayout.setStatusBarScrimColor(
+                    ContextCompat.getColor(this, R.color.colorPrimaryDark)
+            );
+        }
+
     }
 }
